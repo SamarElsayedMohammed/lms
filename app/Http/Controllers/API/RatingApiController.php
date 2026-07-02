38e6368f02df22bb069ecb6d1cfd1b704fcb420c
SamarElsayedMohammed <samarelsayedmohammed12345@gmail.com>
2026-07-02 15:55:56 +0300

    feat: inject ContentAccessService dependency into RatingApiController

--- app/Http/Controllers/API/RatingApiController.php
+++ app/Http/Controllers/API/RatingApiController.php
@@ -7,6 +7,7 @@ use App\Models\Course\Course;
 use App\Models\Order;
 use App\Models\Rating;
 use App\Services\ApiResponseService;
+use App\Services\ContentAccessService;
 use App\Services\FeatureFlagService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
